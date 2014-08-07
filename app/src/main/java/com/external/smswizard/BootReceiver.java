05c5f2495b40fcaeba11956da7eb3374e769c0cb
novikovruslan <novikov.ruslan@gmail.com>
2014-08-07 18:51:23 +0300

    1

--- app/src/main/java/com/external/smswizard/BootReceiver.java
+++ app/src/main/java/com/external/smswizard/BootReceiver.java
@@ -10,23 +10,16 @@ import com.external.smswizard.model.ApplicationModel;
 
 public class BootReceiver extends BroadcastReceiver {
     private static final long DELAY = 10000;
-    private static final String ACTION = "";
-
-    public BootReceiver() {
-    }
 
     @Override
     public void onReceive(Context context, Intent intent) {
         if (new ApplicationModel(context.getApplicationContext()).hasToken()) {
-
-        }
-    }
-
-    public static void schedule(Context context) {
-        AlarmManager am = (AlarmManager) context.getSystemService(Context.ALARM_SERVICE);
-        Intent intent = new Intent(ACTION);
-        PendingIntent pendingIntent = PendingIntent.getBroadcast(context, 0, intent, PendingIntent.FLAG_CANCEL_CURRENT);
+            AlarmManager am = (AlarmManager) context.getSystemService(Context.ALARM_SERVICE);
+            Intent broadcast = new Intent(context, AlarmReceiver.class);
+            PendingIntent pendingIntent = PendingIntent.getBroadcast(context, 0, broadcast, PendingIntent.FLAG_CANCEL_CURRENT);
 //        am.cancel(pendingIntent); // will cancel all alarms whose intent matches this one
-        am.setRepeating(AlarmManager.RTC_WAKEUP, System.currentTimeMillis() + DELAY, DELAY, pendingIntent);
+//            am.setRepeating(AlarmManager.RTC_WAKEUP, System.currentTimeMillis() + DELAY, AlarmManager.INTERVAL_HOUR, pendingIntent);
+            am.setRepeating(AlarmManager.RTC_WAKEUP, System.currentTimeMillis() + DELAY, DELAY, pendingIntent);
+        }
     }
 }
